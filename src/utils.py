7d25bef368f048835ceef7ca9a3be453a8b99fcd
dhdpic <contactdavidhunter@gmail.com>
2019-09-22 15:24:16 +0100

    reverted to orginal files

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,8 @@
 import scipy.misc, numpy as np, os, sys
 
-import imageio
-
 def save_img(out_path, img):
     img = np.clip(img, 0, 255).astype(np.uint8)
     scipy.misc.imsave(out_path, img)
-
 
 def scale_img(style_path, style_scale):
     scale = float(style_scale)
@@ -17,12 +14,11 @@ def scale_img(style_path, style_scale):
 
 def get_img(src, img_size=False):
     img = scipy.misc.imread(src, mode='RGB') # misc.imresize(, (256, 256, 3))
-   
-   if not (len(img.shape) == 3 and img.shape[2] == 3):
-       img = np.dstack((img,img,img))
-   if img_size != False:
-       img = scipy.misc.imresize(img, img_size)
-   return img
+    if not (len(img.shape) == 3 and img.shape[2] == 3):
+        img = np.dstack((img,img,img))
+            if img_size != False:
+                img = scipy.misc.imresize(img, img_size)
+                    return img
 
 def exists(p, msg):
     assert os.path.exists(p), msg
@@ -32,6 +28,5 @@ def list_files(in_path):
     for (dirpath, dirnames, filenames) in os.walk(in_path):
         files.extend(filenames)
         break
-
+    
     return files
-
