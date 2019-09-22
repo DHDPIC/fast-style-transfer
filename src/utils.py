45bc6e0ad87e304fec4edf56f4d1e4fa6ff4ed95
dhdpic <contactdavidhunter@gmail.com>
2019-09-22 15:29:23 +0100

    again reset

--- src/utils.py
+++ src/utils.py
@@ -13,12 +13,12 @@ def scale_img(style_path, style_scale):
     return style_target
 
 def get_img(src, img_size=False):
-    img = scipy.misc.imread(src, mode='RGB') # misc.imresize(, (256, 256, 3))
-    if not (len(img.shape) == 3 and img.shape[2] == 3):
-        img = np.dstack((img,img,img))
-            if img_size != False:
-                img = scipy.misc.imresize(img, img_size)
-                    return img
+   img = scipy.misc.imread(src, mode='RGB') # misc.imresize(, (256, 256, 3))
+   if not (len(img.shape) == 3 and img.shape[2] == 3):
+       img = np.dstack((img,img,img))
+   if img_size != False:
+       img = scipy.misc.imresize(img, img_size)
+   return img
 
 def exists(p, msg):
     assert os.path.exists(p), msg
@@ -28,5 +28,5 @@ def list_files(in_path):
     for (dirpath, dirnames, filenames) in os.walk(in_path):
         files.extend(filenames)
         break
-    
+
     return files
