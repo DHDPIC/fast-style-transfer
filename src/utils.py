d63ee2892b4a068d4b1cc05f6eb716a3ad0f4265
dhdpic <contactdavidhunter@gmail.com>
2019-09-22 11:49:43 +0100

    added imageio

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,11 @@
 import scipy.misc, numpy as np, os, sys
 
+import imageio
+
 def save_img(out_path, img):
     img = np.clip(img, 0, 255).astype(np.uint8)
-    scipy.misc.imsave(out_path, img)
+#scipy.misc.imsave(out_path, img)
+imageio.imread(img)
 
 def scale_img(style_path, style_scale):
     scale = float(style_scale)
